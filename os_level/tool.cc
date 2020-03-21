fe579d745040b12fa17331e03e521ab81d649704
koentjehh <eikelnboomkoen@gmail.com>
2020-03-21 15:13:51 +0100

    aanpassingen n.a.v. feedback

--- os_level/tool.cc
+++ os_level/tool.cc
@@ -1,15 +1,27 @@
 #include <iostream>
 #include <string>
 
-int main(int argc, char *argv[]){
-  string resultaat = "";
+std::string translate(std::string line){
+  std::string result = line;
   
-  for(int i=0; argv.size(); i++){
-    if(argv[i]){ // hoofdletters
-      resultaat += char(int(argv[i]+argc-65)%26 +65);
-    }else{ // kleine letters
-      resultaat += char(int(argv[i]+argc-97)%26 +65);
+  for(int i=0; i < line.size(); i++){
+    if(line[i]>= 65 && line[i] <= 90){ // hoofdletters
+      result[i] = char((line[i] + 3) % (26 + line[i]));
+    }else if(line[i]>97 && line[i] <= 122){ // kleine letters
+      result[i] = char((line[i] + 3) % (26 + line[i]));
     }
   }
-  return resultaat;
+  return result;
 }
+
+int main(int argc, char *argv[])
+{ std::string line;
+
+  if(argc != 2)
+  { std::cerr << "Deze functie heeft exact 1 argument nodig" << std::endl;
+    return -1; }
+
+  while(std::getline(std::cin, line))
+  { std::cout << translate(line) << std::endl; } 
+
+  return 0; }
